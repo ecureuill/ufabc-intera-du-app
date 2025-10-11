05a63eb043d17b51473e2c0b2b890d0419d15415
Camilla Almeida da Silva <c.almeida.da.silva@avanade.com>
2025-10-11 18:22:09 -0300

    Add atividade07

---
+++ atividade07/app/src/main/java/edu/estudos/ufabc/atividade07/MainActivity.kt
@@ -0,0 +1,24 @@
+package edu.estudos.ufabc.atividade07
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        // Safe: ensure the root view exists before attaching the insets listener
+        val rootView = findViewById<android.view.View?>(R.id.main)
+        rootView?.let { view ->
+            ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+}
